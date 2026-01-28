1fc8011b1fb1344ad77983940e28254f66b22e13
Anas-elbourkkadi <elbourkkadianas@gmail.com>
2026-01-28 15:07:34 +0100

    addded the function passwordValidator

--- src/pages/home/parties/createPassword.jsx
+++ src/pages/home/parties/createPassword.jsx
@@ -11,6 +11,7 @@ const CreatePassword = () => {
     const [numb, setNumber] = useState(false);
     const [symbol, setSymbols] = useState(false);
     let [password, setPassword] = useState('...');
+    let [statusPassword, setStatus] = useState('status')
 
     const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const letterUpper = letters.split("");
@@ -27,32 +28,44 @@ const CreatePassword = () => {
     }
 
     let check = () => {
-        password = ''
-        while (password.length < valueRange && (uppercase || lowercase || numb || symbol)) {
+        let newpassword = '';
+        while (newpassword.length < valueRange && (uppercase || lowercase || numb || symbol)) {
 
-            if (uppercase) password += generterPassword(letterUpper);
+            if (uppercase) newpassword += generterPassword(letterUpper);
 
-            if (lowercase) password += generterPassword(letterLower);
+            if (lowercase) newpassword += generterPassword(letterLower);
 
-            if (numb) password += generterPassword(num);
+            if (numb) newpassword += generterPassword(num);
 
-            if (symbol) password += generterPassword(symbols);
+            if (symbol) newpassword += generterPassword(symbols);
 
 
         }
         setPassword(password);
+        passwordValidator();
+    }
+
+    let passwordValidator = () => {
+        let theValues = 0;
+        [numb, lowercase, uppercase, symbol].forEach(e => {
+            e && theValues++
+        });
+
+        if (password.length > 15 && (uppercase && lowercase && numb && symbol)) setStatus("good");
+        else if (password.length > 10 && (theValues >= 2)) setStatus("meduim");
+        else setStatus("the bade password");
     }
 
     return (
         <>
-            <div className="h-screen flex flex-col items-center justify-center capitalize   text-[#cdcbd4]">
+            <div className="h-screen flex flex-col items-center justify-center    text-[#cdcbd4]">
                 <div className="flex flex-col items-center w-[50%] gap-4">
-                    <h1 className="text-[#686672] text-2xl">password genter</h1>
+                    <h1 className="text-[#686672] text-2xl capitalize">password genter</h1>
                     <div className="bg-[#24232b] w-[75%] flex justify-between items-center px-5 py-4">
                         <h1> {password}</h1>
                         <FaRegCopy className=" text-[#a5feb0]" />
                     </div>
-                    <div className="bg-[#24232b] w-[75%] px-5 py-5 shadow-2xl space-y-3">
+                    <div className="bg-[#24232b] w-[75%] px-5 py-5 shadow-2xl space-y-3 capitalize">
                         <div className="flex  justify-between items-center">
                             <p>charcter length</p>
                             <p className="text-[25px] text-[#a5feb0]">{valueRange}</p>
@@ -71,24 +84,24 @@ const CreatePassword = () => {
                         </div>
 
                         <div className=" space-x-3">
-                            <input id="lower" type="checkbox" onChange={(e)=>setLowercase(e.target.checked)} className="accent-[#a5feb0] " />
+                            <input id="lower" type="checkbox" onChange={(e) => setLowercase(e.target.checked)} className="accent-[#a5feb0] " />
                             <label htmlFor="lower">include lowercase letters</label>
                         </div>
 
                         <div className=" space-x-3">
-                            <input id="nu" type="checkbox" onChange={(e)=>setNumber(e.target.checked)} className="accent-[#a5feb0] " />
+                            <input id="nu" type="checkbox" onChange={(e) => setNumber(e.target.checked)} className="accent-[#a5feb0] " />
                             <label htmlFor="nu">include numbers</label>
                         </div>
 
                         <div className=" space-x-3">
-                            <input id="sy" onChange={(e)=>setSymbols(e.target.checked)} type="checkbox" className="accent-[#a5feb0] " />
+                            <input id="sy" onChange={(e) => setSymbols(e.target.checked)} type="checkbox" className="accent-[#a5feb0] " />
                             <label htmlFor="sy">include symbols</label>
                         </div>
 
                         <div className="flex justify-between items-center bg-[#18171f] py-5 px-3 uppercase">
                             <h1 className="text-[20px]">serngth</h1>
                             <div className="flex items-center justify-center">
-                                <p>meduim</p>
+                                <p>{statusPassword}</p>
                                 <PiAlignBottomSimpleBold />
                             </div>
                         </div>
